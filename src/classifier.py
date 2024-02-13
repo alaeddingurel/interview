c136bdcc1e367000280f68f037c36f7b029e039e
Alaeddin Selçuk G <31259349+alaeddingurel@users.noreply.github.com>
2024-02-13 23:26:48 +0300

    main removed
    main removed which is added for testing purposes

--- src/classifier.py
+++ src/classifier.py
@@ -11,28 +11,26 @@ class ZeroShotClassifier:
         return result
 
 
-if __name__ == '__main__':
+# facebook/bart-base
+# facebook/bart-large
+# facebook/bart-large-mnli
 
-    # facebook/bart-base
-    # facebook/bart-large
-    # facebook/bart-large-mnli
+# Full path needed maybe there should be function which takes the directory and the directory in the end of the file
+# model_path = "facebook/bart-large-mnli"
+# zero_shot_classifier = ZeroShotClassifier(model_path)
+#
+# input_text = "I'm really interest on the python programming language and the front-end development"
+#
+# candidate_labels = ["science", "technology", "history", "politics"]
+#
+# classification_result = zero_shot_classifier.classify(input_text, candidate_labels)
+#
+# print("Input Text:", input_text)
+# print("Predicted Label:", classification_result['labels'])
+# print("Confidence Score:", classification_result['scores'])
 
-    # #TODO Full path needed maybe there should be function which takes the directory and the directory in the end of the file
-    # model_path = "facebook/bart-large-mnli"
-    # zero_shot_classifier = ZeroShotClassifier(model_path)
-    #
-    # input_text = "I'm really interest on the python programming language and the front-end development"
-    #
-    # candidate_labels = ["science", "technology", "history", "politics"]
-    #
-    # classification_result = zero_shot_classifier.classify(input_text, candidate_labels)
-    #
-    # print("Input Text:", input_text)
-    # print("Predicted Label:", classification_result['labels'])
-    # print("Confidence Score:", classification_result['scores'])
-
-    #inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-    #outputs = model(**inputs)
+#inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+#outputs = model(**inputs)
 
     #last_hidden_states = outputs.last_hidden_state
 
